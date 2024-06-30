11780a9a5f06784d29816a010183b30aaffb4490
Šimon Brauner <simonbrauner@seznam.cz>
2024-06-30 13:02:23 +0200

    feat(03): part 1

--- AdventOfCode.Solutions/Year2020/Day03/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day03/Solution.cs
@@ -1,13 +1,52 @@
 namespace AdventOfCode.Solutions.Year2020.Day03;
 
+class TreeMap
+{
+    String[] trees;
+
+    public TreeMap(String[] trees)
+    {
+        this.trees = trees;
+    }
+
+    public bool TreeOnCoordinates(int x, int y)
+    {
+        return trees[y][x % trees[0].Length] == '#';
+    }
+
+    public int RowCount()
+    {
+        return trees.Length;
+    }
+}
+
 class Solution : SolutionBase
 {
+    TreeMap treeMap;
+
     public Solution()
-        : base(03, 2020, "") { }
+        : base(03, 2020, "")
+    {
+        treeMap = new TreeMap(Input.SplitByNewline());
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        int x = 0;
+        int y = 0;
+        int result = 0;
+
+        while (++y < treeMap.RowCount())
+        {
+            x += 3;
+
+            if (treeMap.TreeOnCoordinates(x, y))
+            {
+                result++;
+            }
+        }
+
+        return result.ToString();
     }
 
     protected override string SolvePartTwo()
