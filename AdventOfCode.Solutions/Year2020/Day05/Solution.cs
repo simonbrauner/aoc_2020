28107b503aabd32dbc1471602ef5e74751b95d5a
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-03 21:19:30 +0200

    feat(05): part 1

--- AdventOfCode.Solutions/Year2020/Day05/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day05/Solution.cs
@@ -1,13 +1,60 @@
 namespace AdventOfCode.Solutions.Year2020.Day05;
 
+class Seat
+{
+    const int colNumberOffset = 7;
+    const char rowAddLetter = 'B';
+    const char colAddLetter = 'R';
+    int rowNumber = 0;
+    int colNumber = 0;
+
+    public Seat(string textRepresentation)
+    {
+        int power = 1;
+        for (int i = colNumberOffset - 1; i >= 0; i--)
+        {
+            if (textRepresentation[i] == rowAddLetter)
+            {
+                rowNumber += power;
+            }
+
+            power *= 2;
+        }
+
+        power = 1;
+        for (int i = textRepresentation.Length - 1; i >= colNumberOffset; i--)
+        {
+            if (textRepresentation[i] == colAddLetter)
+            {
+                colNumber += power;
+            }
+
+            power *= 2;
+        }
+    }
+
+    public int Id()
+    {
+        return rowNumber * 8 + colNumber;
+    }
+}
+
 class Solution : SolutionBase
 {
+    List<Seat> seats = new List<Seat>();
+
     public Solution()
-        : base(05, 2020, "") { }
+        : base(05, 2020, "")
+    {
+        foreach (string line in Input.SplitByNewline())
+        {
+            seats.Add(new Seat(line));
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return seats.Max(s => s.Id()).ToString();
     }
 
     protected override string SolvePartTwo()
