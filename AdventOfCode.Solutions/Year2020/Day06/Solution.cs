e64e2a65ca8337fb5a0df07fb1ad530b4cf36304
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-04 20:42:18 +0200

    feat(06): part 1

--- AdventOfCode.Solutions/Year2020/Day06/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day06/Solution.cs
@@ -2,12 +2,30 @@ namespace AdventOfCode.Solutions.Year2020.Day06;
 
 class Solution : SolutionBase
 {
+    List<HashSet<char>> yesAnswersByGroups = new List<HashSet<char>>();
+
     public Solution()
-        : base(06, 2020, "") { }
+        : base(06, 2020, "")
+    {
+        foreach (string yesAnswersGroup in Input.SplitByParagraph())
+        {
+            HashSet<char> yesAnswers = new HashSet<char>();
+
+            foreach (char letter in yesAnswersGroup)
+            {
+                if (Char.IsAsciiLetterLower(letter))
+                {
+                    yesAnswers.Add(letter);
+                }
+            }
+
+            yesAnswersByGroups.Add(yesAnswers);
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        return yesAnswersByGroups.Select(a => a.Count()).Sum().ToString();
     }
 
     protected override string SolvePartTwo()
