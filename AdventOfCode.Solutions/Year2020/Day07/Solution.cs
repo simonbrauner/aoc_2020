f732968066c9ea09e5125b9a72cb4beeb270c8d4
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-09 16:07:48 +0200

    feat(07): part 1

--- AdventOfCode.Solutions/Year2020/Day07/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day07/Solution.cs
@@ -1,17 +1,65 @@
 namespace AdventOfCode.Solutions.Year2020.Day07;
 
+using System.Text.RegularExpressions;
+
+record BagContent(int count, string description);
+
 class Solution : SolutionBase
 {
+    const string ShinyGoldName = "shiny gold";
+    Dictionary<string, List<BagContent>> bagContents = new Dictionary<string, List<BagContent>>();
+
     public Solution()
-        : base(07, 2020, "") { }
+        : base(07, 2020, "")
+    {
+        foreach (string line in Input.SplitByNewline())
+        {
+            string bag = Regex.Match(line, @"^(.*?) bags").Groups[1].Value;
+
+            List<BagContent> contents = new List<BagContent>();
+            foreach (Match match in Regex.Matches(line, @"(\d+) (.*?) bag"))
+            {
+                contents.Add(new(int.Parse(match.Groups[1].Value), match.Groups[2].Value));
+            }
+
+            bagContents.Add(bag, contents);
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        HashSet<string> canContainShinyGold = new HashSet<string>();
+
+        foreach (string bag in bagContents.Keys)
+        {
+            TryIncludeShinyGold(bag, ref canContainShinyGold);
+        }
+
+        return canContainShinyGold.Count().ToString();
     }
 
     protected override string SolvePartTwo()
     {
         return "";
+    }
+
+    void TryIncludeShinyGold(string bag, ref HashSet<string> canContainShinyGold)
+    {
+        Queue<string> queue = new Queue<string>(bagContents[bag].Select(c => c.description));
+
+        string? current;
+        while (queue.TryDequeue(out current))
+        {
+            if (current == ShinyGoldName || canContainShinyGold.Contains(current))
+            {
+                canContainShinyGold.Add(bag);
+                return;
+            }
+
+            foreach (BagContent bagContent in bagContents[current])
+            {
+                queue.Enqueue(bagContent.description);
+            }
+        }
     }
 }
