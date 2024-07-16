c4a4c015a423ad0073e6a4a29d33bff4a9bd015d
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-16 21:31:15 +0200

    feat(09): part 1

--- AdventOfCode.Solutions/Year2020/Day09/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day09/Solution.cs
@@ -2,16 +2,49 @@ namespace AdventOfCode.Solutions.Year2020.Day09;
 
 class Solution : SolutionBase
 {
+    const int EarlierNumberCount = 25;
+    List<long> numbers = new List<long>();
+
     public Solution()
-        : base(09, 2020, "") { }
+        : base(09, 2020, "")
+    {
+        foreach (string line in Input.SplitByNewline())
+        {
+            long number = long.Parse(line);
+            numbers.Add(number);
+        }
+    }
 
     protected override string SolvePartOne()
     {
+        for (int i = EarlierNumberCount; i < numbers.Count(); i++)
+        {
+            if (!IsSumOfTwoEarlier(i))
+            {
+                return numbers[i].ToString();
+            }
+        }
         return "";
     }
 
     protected override string SolvePartTwo()
     {
         return "";
+    }
+
+    bool IsSumOfTwoEarlier(int i)
+    {
+        for (int j = i - EarlierNumberCount; j < i; j++)
+        {
+            for (int k = i - EarlierNumberCount; k < i; k++)
+            {
+                if (numbers[j] != numbers[k] && numbers[j] + numbers[k] == numbers[i])
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
