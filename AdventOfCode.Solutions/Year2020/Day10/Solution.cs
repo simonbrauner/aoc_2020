585235b99bf81dab6d7b8f85b59405ec2f17681e
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-17 05:40:10 +0200

    feat(10): part 1

--- AdventOfCode.Solutions/Year2020/Day10/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day10/Solution.cs
@@ -2,12 +2,37 @@ namespace AdventOfCode.Solutions.Year2020.Day10;
 
 class Solution : SolutionBase
 {
+    List<int> joltages = new List<int>();
+
     public Solution()
-        : base(10, 2020, "") { }
+        : base(10, 2020, "")
+    {
+        joltages.Add(0);
+
+        foreach (string line in Input.SplitByNewline())
+        {
+            joltages.Add(int.Parse(line));
+        }
+
+        joltages.Sort();
+        joltages.Add(joltages.Last() + 3);
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        Dictionary<int, int> differences = new Dictionary<int, int>()
+        {
+            { 1, 0 },
+            { 2, 0 },
+            { 3, 0 }
+        };
+
+        for (int i = 1; i < joltages.Count(); i++)
+        {
+            differences[joltages[i] - joltages[i - 1]]++;
+        }
+
+        return (differences[1] * differences[3]).ToString();
     }
 
     protected override string SolvePartTwo()
