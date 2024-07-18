09cb105dc3aad79a120bb442e5d63fcf329e82ef
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-18 21:55:30 +0200

    feat(11): part 2

--- AdventOfCode.Solutions/Year2020/Day11/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day11/Solution.cs
@@ -4,32 +4,35 @@ class SeatGrid
 {
     List<StringBuilder> currentGrid = new List<StringBuilder>();
     List<StringBuilder> nextGrid = new List<StringBuilder>();
+    int part;
 
-    public SeatGrid(string[] lines)
+    public SeatGrid(string[] lines, int part)
     {
         foreach (string line in lines)
         {
             currentGrid.Add(new StringBuilder(line));
             nextGrid.Add(new StringBuilder(line));
         }
+
+        this.part = part;
     }
 
-    public int RowCount()
+    int RowCount()
     {
         return currentGrid.Count();
     }
 
-    public int ColCount()
+    int ColCount()
     {
         return currentGrid[0].Length;
     }
 
-    public bool IsOccupiedSeat(int x, int y)
+    bool IsOccupiedSeat(int x, int y)
     {
         return currentGrid[y][x] == '#';
     }
 
-    public bool IsEmptySeat(int x, int y)
+    bool IsEmptySeat(int x, int y)
     {
         return currentGrid[y][x] == 'L';
     }
@@ -44,41 +47,59 @@ class SeatGrid
         return x >= 0 && x < ColCount();
     }
 
-    public void AdjacentCounts(int x, int y, out int emptyCount, out int occupiedCount)
+    bool IsAdjacentOccupied(int x, int y, int dx, int dy)
     {
-        emptyCount = 0;
-        occupiedCount = 0;
+        if (part == 1)
+        {
+            return RowValid(y + dy) && ColValid(x + dx) && IsOccupiedSeat(x + dx, y + dy);
+        }
+
+        while (true)
+        {
+            x += dx;
+            y += dy;
+
+            if (!RowValid(y) || !ColValid(x) || IsEmptySeat(x, y))
+            {
+                return false;
+            }
+
+            if (IsOccupiedSeat(x, y))
+            {
+                return true;
+            }
+        }
+    }
+
+    int AdjacentOccupiedCount(int x, int y)
+    {
+        int occupiedCount = 0;
 
         for (int dx = -1; dx <= 1; dx++)
         {
             for (int dy = -1; dy <= 1; dy++)
             {
-                if ((dx != 0 || dy != 0) && RowValid(y + dy) && ColValid(x + dx))
+                if ((dx != 0 || dy != 0) && IsAdjacentOccupied(x, y, dx, dy))
                 {
-                    if (IsEmptySeat(x + dx, y + dy))
-                    {
-                        emptyCount++;
-                    }
-                    else if (IsOccupiedSeat(x + dx, y + dy))
-                    {
-                        occupiedCount++;
-                    }
+                    occupiedCount++;
                 }
             }
         }
+
+        return occupiedCount;
     }
 
-    public void OccupySeat(int x, int y)
+    void OccupySeat(int x, int y)
     {
         nextGrid[y][x] = '#';
     }
 
-    public void EmptySeat(int x, int y)
+    void EmptySeat(int x, int y)
     {
         nextGrid[y][x] = 'L';
     }
 
-    public int OccupiedCount()
+    int OccupiedCount()
     {
         int occupiedCount = 0;
 
@@ -96,7 +117,7 @@ class SeatGrid
         return occupiedCount;
     }
 
-    public bool Converged()
+    bool Converged()
     {
         for (int x = 0; x < ColCount(); x++)
         {
@@ -112,7 +133,7 @@ class SeatGrid
         return true;
     }
 
-    public void Update()
+    void Update()
     {
         for (int x = 0; x < ColCount(); x++)
         {
@@ -120,6 +141,41 @@ class SeatGrid
             {
                 currentGrid[y][x] = nextGrid[y][x];
             }
+        }
+    }
+
+    bool SeatBecomesEmpty(int occupiedCount)
+    {
+        return (part == 1 && occupiedCount >= 4) || (part == 2 && occupiedCount >= 5);
+    }
+
+    public int Solve()
+    {
+        while (true)
+        {
+            for (int x = 0; x < ColCount(); x++)
+            {
+                for (int y = 0; y < RowCount(); y++)
+                {
+                    int occupiedCount = AdjacentOccupiedCount(x, y);
+
+                    if (IsEmptySeat(x, y) && occupiedCount == 0)
+                    {
+                        OccupySeat(x, y);
+                    }
+                    else if (IsOccupiedSeat(x, y) && SeatBecomesEmpty(occupiedCount))
+                    {
+                        EmptySeat(x, y);
+                    }
+                }
+            }
+
+            if (Converged())
+            {
+                return OccupiedCount();
+            }
+
+            Update();
         }
     }
 
@@ -139,48 +195,16 @@ class SeatGrid
 
 class Solution : SolutionBase
 {
-    SeatGrid seatGrid;
-
     public Solution()
-        : base(11, 2020, "")
-    {
-        seatGrid = new SeatGrid(Input.SplitByNewline());
-    }
+        : base(11, 2020, "") { }
 
     protected override string SolvePartOne()
     {
-        while (true)
-        {
-            for (int x = 0; x < seatGrid.ColCount(); x++)
-            {
-                for (int y = 0; y < seatGrid.RowCount(); y++)
-                {
-                    int emptyCount;
-                    int occupiedCount;
-                    seatGrid.AdjacentCounts(x, y, out emptyCount, out occupiedCount);
-
-                    if (seatGrid.IsEmptySeat(x, y) && occupiedCount == 0)
-                    {
-                        seatGrid.OccupySeat(x, y);
-                    }
-                    else if (seatGrid.IsOccupiedSeat(x, y) && occupiedCount >= 4)
-                    {
-                        seatGrid.EmptySeat(x, y);
-                    }
-                }
-            }
-
-            if (seatGrid.Converged())
-            {
-                return seatGrid.OccupiedCount().ToString();
-            }
-
-            seatGrid.Update();
-        }
+        return new SeatGrid(Input.SplitByNewline(), 1).Solve().ToString();
     }
 
     protected override string SolvePartTwo()
     {
-        return "";
+        return new SeatGrid(Input.SplitByNewline(), 2).Solve().ToString();
     }
 }
