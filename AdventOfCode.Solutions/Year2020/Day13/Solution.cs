522d42926678b8f76da4092b6eaf967b7094000a
Šimon Brauner <simonbrauner@seznam.cz>
2024-07-19 15:06:26 +0200

    feat(13): part 1

--- AdventOfCode.Solutions/Year2020/Day13/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day13/Solution.cs
@@ -1,17 +1,48 @@
 namespace AdventOfCode.Solutions.Year2020.Day13;
 
+using System.Text.RegularExpressions;
+
 class Solution : SolutionBase
 {
+    int earliestTimestamp;
+    List<int> departures = new List<int>();
+
     public Solution()
-        : base(13, 2020, "") { }
+        : base(13, 2020, "")
+    {
+        string[] lines = Input.SplitByNewline();
+
+        earliestTimestamp = int.Parse(lines[0]);
+        foreach (Match match in Regex.Matches(lines[1], @"\d+"))
+        {
+            departures.Add(int.Parse(match.Value));
+        }
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        int minBus = departures[0];
+        int minWaiting = WaitingTime(departures[0]);
+
+        foreach (int departure in departures)
+        {
+            if (WaitingTime(departure) < minWaiting)
+            {
+                minBus = departure;
+                minWaiting = WaitingTime(departure);
+            }
+        }
+
+        return (minBus * minWaiting).ToString();
     }
 
     protected override string SolvePartTwo()
     {
         return "";
+    }
+
+    int WaitingTime(int departure)
+    {
+        return departure - (earliestTimestamp % departure);
     }
 }
