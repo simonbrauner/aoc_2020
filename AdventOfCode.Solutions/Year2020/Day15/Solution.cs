dcb1ad79bd3a9fef3ead31348cebd73c23b9405e
Šimon Brauner <simonbrauner@seznam.cz>
2024-08-02 08:43:47 +0200

    feat(15): part 1

--- AdventOfCode.Solutions/Year2020/Day15/Solution.cs
+++ AdventOfCode.Solutions/Year2020/Day15/Solution.cs
@@ -2,12 +2,44 @@ namespace AdventOfCode.Solutions.Year2020.Day15;
 
 class Solution : SolutionBase
 {
+    List<int> startingNumbers;
+    Dictionary<int, int> mostRecentlySpoken = new Dictionary<int, int>();
+
     public Solution()
-        : base(15, 2020, "") { }
+        : base(15, 2020, "")
+    {
+        startingNumbers = new List<int>(Input.Split(',').Select(s => int.Parse(s)));
+    }
 
     protected override string SolvePartOne()
     {
-        return "";
+        int turn = 0;
+
+        foreach (int startingNumber in startingNumbers)
+        {
+            turn++;
+            mostRecentlySpoken.Add(startingNumber, turn);
+        }
+
+        int current = startingNumbers.Last();
+        while (turn < 2020)
+        {
+            int mostRecentlySpokenCurrent = mostRecentlySpoken.GetValueOrDefault(current);
+            mostRecentlySpoken[current] = turn;
+
+            if (mostRecentlySpokenCurrent == 0)
+            {
+                current = 0;
+            }
+            else
+            {
+                current = turn - mostRecentlySpokenCurrent;
+            }
+
+            turn++;
+        }
+
+        return current.ToString();
     }
 
     protected override string SolvePartTwo()
